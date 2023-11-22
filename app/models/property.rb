7c3faee8a4124322bc464be5a183ce9477ee0909
David Florez <dflorez6@gmail.com>
2023-11-21 19:24:54 -0500

    v 1.0.6
    # MOBILE APPS FINAL PROJECT API v1
    
    ## BACK-END
    * Scaffold - Property
    * OwnerModel - added association to Properties
    * PropertiesController - CRUD actions + return JSON objects
    * PropertyModel - associations: belongs to owner + default scope + method to display full address
    * Routes - properties nested under owners (/owners/:id/properties)
    * Migrations
    
    ## PENDING
    *

---
+++ app/models/property.rb
@@ -0,0 +1,27 @@
+class Property < ApplicationRecord
+  #====================
+  # Validations
+  #====================
+
+  #====================
+  # Associations
+  #====================
+  # Many-to-Many
+  # has_many :applications
+  # has_many :prospects, through: :applications
+
+  # Belongs_to
+  belongs_to :owner
+
+  #====================
+  # Scopes
+  #====================
+  default_scope { order(created_at: :desc) }
+
+  #====================
+  # Method
+  #====================
+  def full_address
+    "#{self.street}, #{self.city}, #{self.postal_code}, #{self.province}, #{self.country}"
+  end
+end
