1d13a6b18264fdbe944df88e9fdb2cba5adda119
David Florez <dflorez6@gmail.com>
2023-11-28 23:06:13 -0500

    v 1.0.19 # MOBILE APPS FINAL PROJECT API v1
    ## BACK-END
    * Minor refactor to CORS Initializer
    
    ## PENDING
    * CORS -  Find out how to accept incoming requests from the app once its in production
    * Encrypt Password

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # TODO: For now Im on development environment so I will accept incoming requests from ng serve only. Find a solution for the app in production
     # origins 'http://localhost:4200' # You can set specific origins if needed, e.g., 'http://localhost:3000'
     # TODO: According to GPT to do it with an APK angular/cordova
-    origins 'content://', 'file://', /\Ahttp:\/\/localhost(:\d+)?\z/
+    origins 'content://', 'file://', /\Ahttp(s)?:\/\/localhost(:\d+)?\z/
 
     resource '*',
              headers: :any,
