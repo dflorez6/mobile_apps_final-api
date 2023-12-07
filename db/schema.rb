206de4c6bbfe62ecd940dba4c0328bba729e014f
David Florez <dflorez6@gmail.com>
2023-12-07 03:45:45 -0500

    v 1.0.37 # MOBILE APPS FINAL PROJECT API v1
    ## BACK-END
    * Migration created to update default Country value from Property
    
    ## PENDING
    *

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_002707) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_084251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_002707) do
     t.string "city"
     t.string "postal_code"
     t.string "province"
-    t.string "country"
+    t.string "country", default: "Canada"
     t.float "latitude"
     t.float "longitude"
     t.boolean "is_apartment"
